61f534840997ed3b28c00bce7188a7fd4e590b51
shon0921 <0921shon@naver.com>
2024-03-11 14:46:42 +0900

    University for Practice

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello World332!");
+        System.out.println("Hello World331!");
         System.out.println("Hello!! My First Java Coding!!");
     }
 }
