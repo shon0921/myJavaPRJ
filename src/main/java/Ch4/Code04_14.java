e5e14ababd2593ea7e16d05793cb020469106622
shon0921 <0921shon@naver.com>
2024-03-25 14:29:23 +0900

    indexOf

---
+++ src/main/java/Ch4/Code04_14.java
@@ -0,0 +1,12 @@
+package Ch4;
+
+public class Code04_14 {
+    public static void main(String[] args) {
+        String str = "난생처음 자바";
+
+        System.out.println(str.indexOf("난생"));
+        System.out.println(str.lastIndexOf("처"));
+        System.out.println(str.indexOf("처음",4));
+    }
+
+}
