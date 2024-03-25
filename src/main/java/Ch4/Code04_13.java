2ceccda5094ffab0f1bbb443384e222210c31287
shon0921 <0921shon@naver.com>
2024-03-25 14:56:15 +0900

    문자열 앞뒤의 공백을 잘라내는 메서드

--- src/main/java/Ch4/Code04_13.java
+++ src/main/java/Ch4/Code04_13.java
@@ -4,10 +4,15 @@ public class Code04_13 {
     public static void main(String[] args) {
         String str = "  한글 ABCD efgh";
         String cutStr="";
+        String lower="";
+        String upper = "";
+        String allStr ="";
 
         cutStr = str.trim();
+        allStr = str.replaceAll(" ","");
 
         System.out.println("기존 문자열 ==> [" + str + "]");
         System.out.println("공백 제거 ==> [" + cutStr + "]");
+        System.out.println("모든 공백 제거 ==> ["+allStr+"]");
     }
 }
