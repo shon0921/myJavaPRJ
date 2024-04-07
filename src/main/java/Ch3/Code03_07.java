47e443da7ae8b19188a0f22af1ab4df476419541
shon0921 <43308838+shon0921@users.noreply.github.com>
2024-04-07 18:01:12 +0900

    Add files via upload

---
+++ src/main/java/Ch3/Code03_07.java
@@ -0,0 +1,11 @@
+package Other2;
+
+public class Code03_07 {
+    public static void main(String[] args) {
+        int num;
+        num = 100;
+        num = 100 * 200;
+        num = Integer.parseInt("100")+Integer.parseInt("200");
+        System.out.println(num);
+    }
+}
