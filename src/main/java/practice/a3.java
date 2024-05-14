448f0eaf792a06deff900724ebeb039a85a35671
shon0921 <43308838+shon0921@users.noreply.github.com>
2024-05-14 23:52:49 +0900

    Add files via upload

---
+++ src/main/java/practice/a3.java
@@ -0,0 +1,24 @@
+package Other2;
+    class Rabbit3{
+        int t;
+        int xpos;
+        int ypos;
+
+        Rabbit3(int v) {this.t=v;}
+        void set(int x,int y)
+        {
+            this.xpos=x;
+            this.ypos=y;
+
+        }
+    }
+
+    public class a3 {
+        public static void main(String[] args) {
+            Other2.Rabbit3 rabbit1 = new Other2.Rabbit3(100);
+
+
+        }
+    }
+
+
