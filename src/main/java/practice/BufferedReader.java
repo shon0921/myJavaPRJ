90d6853de07e4a9661d756ee92ef2c4f84ac7bd1
shon0921 <43308838+shon0921@users.noreply.github.com>
2024-06-06 23:39:11 +0900

    BufferedReader 연습

--- src/main/java/practice/BufferedReader.java
+++ src/main/java/practice/BufferedReader.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class TTT {
+public class BufferedReader {
     public static void main(String[] args) throws IOException { //필수
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 등록
